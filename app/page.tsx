1153b2c45e159e4c00e233954de7bc6032b0273c
Nabeel Husain <nabeel.husain@student.manchester.ac.uk>
2023-05-26 22:12:55 +0100

    Create app and navbar

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className='max-w-full opacity-80 min-h-50'>
+      <img className='object-cover' src='./1.jpg'/>
+    </div>
+  )
+}
