784e89a8501d8ac5e970e497c18816f600e994dc
Nabeel Husain <nabeel.husain@student.manchester.ac.uk>
2023-05-27 00:30:08 +0100

    Add hero card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
+import Hero from '../components/hero';
 
 export default function Home() {
   return (
-    <div className='max-w-full opacity-80 min-h-500'>
-      <img className='object-contain' src='./1.jpg'/>
-    </div>
+    <Hero />
   )
 }
