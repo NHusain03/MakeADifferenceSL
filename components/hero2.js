aa4ffb47469975d2e92f0856975ab5e9a37dc998
Nabeel Husain <nabeel.husain@student.manchester.ac.uk>
2023-05-31 21:05:21 +0100

    Small refactor

--- components/hero2.js
+++ components/hero2.js
@@ -7,11 +7,11 @@ const Hero2 = () => {
             <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center drop-shadow-lg'>
                 <div className="w-1/3 p-6 bg-slred rounded-lg">
                     <a href="#">
-                        <h5 class="mb-2 py-2 text-4xl font-bold tracking-tight text-white">Make A Difference Sri Lanka</h5>
+                        <h5 className="mb-2 py-2 text-4xl font-bold tracking-tight text-white">Make A Difference Sri Lanka</h5>
                     </a>
-                    <p class="mb-3 py-2 font-light text-white">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p className="mb-3 py-2 font-light text-white">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                     <div className='flex items-center justify-center'>
-                        <a href="#" class="inline-flex items-center px-3 py-2 text-base font-medium text-center text-slred bg-slyellow rounded-lg border-2 border-transparent hover:bg-slred hover:text-slyellow hover:border-slyellow transition-colors duration-300 ease-in-out-quad">
+                        <a href="#" className="inline-flex items-center px-3 py-2 text-base font-medium text-center text-slred bg-slyellow rounded-lg border-2 border-transparent hover:bg-slred hover:text-slyellow hover:border-slyellow transition-colors duration-300 ease-in-out-quad">
                             DONATE
                         </a>
                     </div>
--- components/restOfHome.js
+++ components/restOfHome.js
@@ -5,8 +5,8 @@ const RestOfHome = () => {
     <div className='flex items-center justify-center m-5 space-between-5'>
       <div className='flex-col items-center justify-center'>
         <h1 className='text-xl pb-5 font-bold underline underline-offset-8 text-center'>WHO WE ARE </h1>
-        <div href="#" class="block max-w-sm p-6 bg-slgreen border border-gray-200 rounded-lg shadow">
-          <p class="font-light text-white pb-3">
+        <div href="#" className="block max-w-sm p-6 bg-slgreen border border-gray-200 rounded-lg shadow">
+          <p className="font-light text-white pb-3">
             Lorem ipsum dolor sit amet, consectetur
             adipiscing elit. Nam dignissim leo tristique
             fermentum dignissim.
@@ -21,7 +21,7 @@ const RestOfHome = () => {
             Mauris sodales finibus nisi id faucibus.
           </p>
           <div className='flex items-center justify-center'>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-base font-medium text-center text-slgreen bg-white rounded-lg border-2 border-transparent hover:bg-slgreen hover:text-white hover:border-white transition-colors duration-300 ease-in-out-quad">
+            <a href="#" className="inline-flex items-center px-3 py-2 text-base font-medium text-center text-slgreen bg-white rounded-lg border-2 border-transparent hover:bg-slgreen hover:text-white hover:border-white transition-colors duration-300 ease-in-out-quad">
               CONTACT US
             </a>
           </div>
@@ -30,8 +30,8 @@ const RestOfHome = () => {
 
       <div className='flex-col items-center justify-center m-5'>
         <h1 className='text-xl pb-5 font-bold underline underline-offset-8 text-center'>GALLERY</h1>
-        <div href="#" class="block max-w-sm p-6 bg-slgreen border border-gray-200 rounded-lg shadow">
-          <p class="font-light text-white pb-3">
+        <div href="#" className="block max-w-sm p-6 bg-slgreen border border-gray-200 rounded-lg shadow">
+          <p className="font-light text-white pb-3">
             Lorem ipsum dolor sit amet, consectetur
             adipiscing elit. Nam dignissim leo tristique
             fermentum dignissim.
@@ -46,7 +46,7 @@ const RestOfHome = () => {
             Mauris sodales finibus nisi id faucibus.
           </p>
           <div className='flex items-center justify-center'>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-base font-medium text-center text-slgreen bg-white rounded-lg border-2 border-transparent hover:bg-slgreen hover:text-white hover:border-white transition-colors duration-300 ease-in-out-quad">
+            <a href="#" className="inline-flex items-center px-3 py-2 text-base font-medium text-center text-slgreen bg-white rounded-lg border-2 border-transparent hover:bg-slgreen hover:text-white hover:border-white transition-colors duration-300 ease-in-out-quad">
               CONTACT US
             </a>
           </div>
