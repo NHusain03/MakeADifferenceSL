60efa12d9d19e2cc8dab63443f9a9c54a4b14632
Nabeel Husain <nabeel.husain@student.manchester.ac.uk>
2023-06-28 16:36:06 +0300

    Refactor navbar for mobile optimisation

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,18 +1,45 @@
 'use client';
-import { Navbar, Button, Link, Text } from "@nextui-org/react";
+import { Navbar, Link, Text } from "@nextui-org/react";
 
 const Navigation = () => {
+    const collapseItems = [
+        "Home",
+        "Projects",
+        "Gallery",
+        "Contact Us"
+    ];
+
+    const collapseLinks = [
+        "/",
+        "/projects",
+        "#",
+        "/contact"
+    ]
+
     return (
-        <Navbar isBordered variant={"sticky"} maxWidth={"fluid"} css={{
+        <Navbar isBordered variant="sticky" css={{
             $$navbarBackgroundColor: "#FFBE29",
-            $$navbarBlurBackgroundColor: "#FFBE29"
-          }}>
-            <Navbar.Brand>
-                <Text b color="#00534E" hideIn="xs">
-                    Make A Difference SL
+            $$navbarBlurBackgroundColor: "#FFBE29",
+            color: "#00534E"
+        }}>
+            <Navbar.Toggle showIn="xs" />
+            <Navbar.Brand
+                css={{
+                    "@xs": {
+                        w: "12%",
+                        jc: "left"
+                    },
+                }}
+            >
+                <Text b color="inherit" hideIn={"xs"}>
+                    MDSL
                 </Text>
             </Navbar.Brand>
-            <Navbar.Content hideIn="xs" style={{color: "#00534E"}}>
+            <Navbar.Content
+                enableCursorHighlight
+                hideIn="xs"
+                variant="default"
+            >
                 <Navbar.Link href="/">Home</Navbar.Link>
                 <Navbar.Link href="/projects">Projects</Navbar.Link>
                 <Navbar.Link href="#">Gallery</Navbar.Link>
@@ -20,16 +47,27 @@ const Navigation = () => {
             </Navbar.Content>
             <Navbar.Content>
                 <Navbar.Item>
-                    <Button auto flat as={Link} href="#" style={{
-                        color: "#FFFFFF",
-                        backgroundColor: "#8D153A"
-                        }}> 
-                        DONATE
-                    </Button>
+                    <a className="bg-slred text-xs text-white p-3 rounded-lg sm:ml-10 sm:text-md">DONATE</a>
                 </Navbar.Item>
             </Navbar.Content>
+            <Navbar.Collapse disableAnimation>
+                {collapseItems.map((item, index) => (
+                    <Navbar.CollapseItem key={index}
+                    >
+                        <Link
+                            color="inherit"
+                            css={{
+                                minWidth: "100%",
+                            }}
+                            href={collapseLinks[index]}
+                        >
+                            {item}
+                        </Link>
+                    </Navbar.CollapseItem>
+                ))}
+            </Navbar.Collapse>
         </Navbar>
-    )
+    );
 }
 
 export default Navigation
